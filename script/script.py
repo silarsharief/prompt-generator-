f823e3ffb9d71486954bc7279f3998e451dca4df
silarsharief <69467565+silarsharief@users.noreply.github.com>
2025-05-17 18:48:15 +0530

    Update script.py

--- script/script.py
+++ script/script.py
@@ -462,7 +462,7 @@ import re
 
 llm = ChatGroq(
     groq_api_key=os.getenv("GROQ_API_KEY"),
-    model_name="meta-llama/llama-4-scout-17b-16e-instruct"
+    model_name="deepseek-r1-distill-llama-70b"
 )
 
 
